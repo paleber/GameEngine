c25f11a92e65301c32ca4d47b03320338ae248ce
Patrick Leber <patrick.leber@freenet.de>
2015-07-23 00:50:51 +0200

    add Polygon interface

--- src/basic/IPolygon.java
+++ src/basic/IPolygon.java
@@ -2,4 +2,16 @@ package basic;
 
 public interface IPolygon {
 
+	void initWithPoints(IPoint... points);
+	
+	void initByCopying(IPolygon other);
+	
+	void move(IVector movement);
+	
+	void rotate(IPoint pivot);
+	
+	void getPointIterator();
+	
+	void getEdgeIterator();
+	
 }
