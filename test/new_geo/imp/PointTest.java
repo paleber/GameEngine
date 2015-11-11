bb5831f1fc88595fcfefff324604ed258513ef80
Patrick Leber <patrick.leber@freenet.de>
2015-11-11 20:19:23 +0100

    add initial test

---
+++ test/new_geo/imp/PointTest.java
@@ -0,0 +1,124 @@
+package new_geo.imp;
+
+
+import new_geo.IPoint;
+import new_geo.IVector;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import new_geo.ICircle;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PointTest {
+
+    private static final double DELTA = 1e-9;
+
+    @Test
+    public void testConstructorDefault() {
+        Point p = new Point();
+        assertEquals(0, p.x, DELTA);
+        assertEquals(0, p.y, DELTA);
+    }
+
+
+
+
+
+
+    /*
+    private static final Injector INJECTOR = Guice
+            .createInjector(new GeoModule());
+    private static final double DELTA = 1e-9;
+
+    private final basic.IPoint p = INJECTOR.getInstance(basic.IPoint.class);
+    private final basic.IPoint q = INJECTOR.getInstance(IPoint.class);
+
+    @Test
+    public void initWithXY() {
+        p.initWithXY(3, 4);
+        assertEquals(3, p.getX(), DELTA);
+        assertEquals(4, p.getY(), DELTA);
+    }
+
+    @Test
+    public void initByCopying() {
+        q.initWithXY(2, 3);
+        p.initByCopying(q);
+        assertEquals(2, p.getX(), DELTA);
+        assertEquals(3, p.getY(), DELTA);
+    }
+
+    @Test
+    public void move() {
+        IVector v = INJECTOR.getInstance(IVector.class);
+        v.initWithXY(2, 3);
+
+        p.initWithXY(3, 4);
+        p.move(v);
+
+        assertEquals(5, p.getX(), DELTA);
+        assertEquals(7, p.getY(), DELTA);
+    }
+
+    @Test
+    public void rotate() {
+        q.initWithXY(1, 1);
+        p.initWithXY(0, 0);
+
+        p.rotate(q, 0.5 * Math.PI);
+        assertEquals(2, p.getX(), DELTA);
+        assertEquals(0, p.getY(), DELTA);
+
+        p.rotate(q, 2.5 * Math.PI);
+        assertEquals(2, p.getX(), DELTA);
+        assertEquals(2, p.getY(), DELTA);
+
+        p.rotate(q, -3.5 * Math.PI);
+        assertEquals(0, p.getX(), DELTA);
+        assertEquals(2, p.getY(), DELTA);
+
+    }
+
+    @Test
+    public void xyMinMax() {
+        p.initWithXY(5, 7);
+        assertEquals(5, p.getXMin(), DELTA);
+        assertEquals(5, p.getXMax(), DELTA);
+        assertEquals(7, p.getYMin(), DELTA);
+        assertEquals(7, p.getYMax(), DELTA);
+    }
+
+    @Test
+    public void parents() {
+        ICircle c = INJECTOR.getInstance(ICircle.class);
+        
+        p.initWithXY(2, 3);
+        q.initWithXY(4, 5);
+
+        c.init(p, 1);
+        c.init(q, 1);
+
+        assertEquals(4, c.getMid().getX(), DELTA);
+        /*
+         * ILine l = INJECTOR.getInstance(ILine.class); p.initWithXY(1, 2);
+         * q.initWithXY(4, 3); l.initWithPoints(p, q);
+         * 
+         * IVector v = INJECTOR.getInstance(IVector.class); v.initWithXY(1, 1);
+         * 
+         * //p.move(v);
+         * 
+         * assertEquals(2, l.getXMin(), DELTA); assertEquals(3, l.getYMin(),
+         * DELTA);
+         * 
+         * //
+         *//*
+    }
+
+    @Test
+    public void toStringTest() {
+        p.initWithXY(7, 8);
+        assertEquals("(7.000|8.000)", p.toString());
+    } */
+
+}
